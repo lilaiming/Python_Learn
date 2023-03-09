a9c80102a023417ee0e615349ff76b4ebb99583d
lilaiming <441314423@qq.com>
2023-03-09 15:57:46 +0800

    12

--- README.md
+++ README.md
@@ -1,12 +1,4 @@
+# Python_Learning
 # -*- coding=utf-8 -*-
 # 本脚由lilaiming编写，用于学习使用！
 # Email:essid@qq.com
-
-
-import os
-
-qytang1 = open('qytang1.txt', 'w')
-
-qytang1.write('test file 2123\n')
-
-
--- print.py
+++ print.py
@@ -2,11 +2,20 @@
 # 本脚由lilaiming编写，用于学习使用！
 # Email:essid@qq.com
 
+
 print('welcome to qytang!')
 print('welcome to lenovo!')
 print('welcome to python!')
 print('github commit 1')
 print('github commit 12')
+
+
+
+
+
+
+
+
 
 
 

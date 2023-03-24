c592210f3bcdd3fb8307e675e79f10cb55def0b3
lilaiming <441314423@qq.com>
2023-03-24 17:15:55 +0800

    121

--- dashu.py
+++ dashu.py
@@ -18,12 +18,69 @@
 #
 
 
-def URL1(ip, protocol, url):
-    x = '{}:{}{}'.format(protocol, ip, url)
-    return x
+# def URL1(ip, protocol, url):
+#     x = '{}:{}{}'.format(protocol, ip, url)
+#     return x
+#
+# y = URL1('10.1.1.1','https','qytang666')
+# print(y)
+#
 
-y = URL1('10.1.1.1','https','qytang666')
-print(y)
+# a = [0,1,2,3,4,5,6,7,8,9,10]
+#
+# for baobao in a :
+#     print (baobao)
+
+# a = [0,1,2,3,4,5,6,7,8,9,10,'cisco']
+#
+# b = a.index(9)
+# print (b)
+#
+# c = a.count(0)
+# print (c)
+#
+
+
+# ip_list = []
+#
+# prefix = '10.1.1.'
+# perfix_len = '/24'
+#
+# for i in range(1,9):
+#     ip = prefix + str(i) + perfix_len
+#     ip_list.append(ip)
+#
+# print(ip_list)
+#
+# abc = len(ip_list)
+# print (abc)
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

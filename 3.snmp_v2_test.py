b43065d550b5760abf273c08283c932be9e88fba
lilaiming <441314423@qq.com>
2024-03-19 12:08:40 +0800

    snmpv2v3

--- 3.snmp_v2_test.py
+++ 3.snmp_v2_test.py
@@ -3,6 +3,13 @@
 # Email:essid@qq.com
 
 from pysnmp.hlapi import *
+from concurrent.futures import ThreadPoolExecutor
+
+# 从文件中读取IP地址列表
+def read_ip_list(filename):
+    with open(filename, 'r') as file:
+        ip_list = file.read().splitlines()
+    return ip_list
 
 # 测试SNMP v2
 def test_snmp_v2(ip, community):
@@ -15,11 +22,12 @@ def test_snmp_v2(ip, community):
     )
 
     if errorIndication:
-        print(f"SNMP v2 测试失败：{errorIndication}")
+        print(f"{ip} SNMP v2 测试失败：- {errorIndication}")
     elif errorStatus:
-        print(f"SNMP v2 测试失败：{errorStatus.prettyPrint()} at {errorIndex}")
+        print(f"{ip} SNMP v2 测试失败：- {errorStatus.prettyPrint()} at {errorIndex}")
     else:
-        print(f"SNMP v2 测试成功：{varBinds[0][1].prettyPrint()}")
+        print(f"{ip} SNMP v2 测试成功：")
+        # print(f"SNMP v2 测试成功：{ip} - {varBinds[0][1].prettyPrint()}")
         # 获取设备名称
         get_device_name(ip, community)
 
@@ -45,9 +53,18 @@ def get_device_name(ip, community):
         print(f"设备名称：{varBinds[0][1].prettyPrint()}")
 
 
-# 测试SNMP v2
-ip = '192.168.56.10'
-community = 'P@ssw0rd'
-test_snmp_v2(ip, community)
+# 读取IP地址列表文件
+ip_list = read_ip_list('2.ip_list.txt')
 
+# 使用多线程处理每个IP地址
+with ThreadPoolExecutor(max_workers=5) as executor:
+    for ip in ip_list:
+        community = 'Password'  # 输入交换机配置的community
+        executor.submit(test_snmp_v2, ip, community)
+
+# 交换机配置：
+# snmp-agent
+# snmp-agent trap enable
+# snmp-agent sys-info version v2c
+# snmp-agent community read cipher Password
 
--- 3.snmp_v3_test.py
+++ 3.snmp_v3_test.py
@@ -3,93 +3,98 @@
 # Email:essid@qq.com
 
 from pysnmp.hlapi import *
+from concurrent.futures import ThreadPoolExecutor
+
+# 从文件中读取IP地址列表
+def read_ip_list(filename):
+    with open(filename, 'r') as file:
+        ip_list = file.read().splitlines()
+    return ip_list
 
 # 测试SNMP v3
-def test_snmp_v3(ip, username, auth_password, priv_password, auth_protocol='sha', priv_protocol='aes256'):
-    auth_protocol_map = {
-        'md5': usmHMACMD5AuthProtocol,
-        'sha': usmHMACSHAAuthProtocol
-    }
-    priv_protocol_map = {
-        'des': usmDESPrivProtocol,
-        'aes128': usmAesCfb128Protocol,
-        'aes192': usmAesCfb192Protocol,
-        'aes256': usmAesCfb256Protocol
-    }
+def test_snmp_v3(ip, username, auth_protocol, auth_password, priv_protocol, priv_password):
+    target = UdpTransportTarget((ip, 161))
+    user = UsmUserData(userName=username,
+                       authProtocol=auth_protocol,
+                       authKey=auth_password,
+                       privProtocol=priv_protocol,
+                       privKey=priv_password)
+    oid = ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0))
 
-    auth_protocol = auth_protocol_map.get(auth_protocol, usmHMACSHAAuthProtocol)
-    priv_protocol = priv_protocol_map.get(priv_protocol, usmAesCfb256Protocol)
-
-    errorIndication, errorStatus, errorIndex, varBinds = next(
-        getCmd(SnmpEngine(),
-               UsmUserData(
-                   userName=username,
-                   authKey=auth_password,
-                   privKey=priv_password,
-                   authProtocol=auth_protocol,
-                   privProtocol=priv_protocol
-               ),
-               UdpTransportTarget((ip, 161)),
-               ContextData(),
-               ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)))
-    )
+    errorIndication, errorStatus, errorIndex, varBinds = next(getCmd(SnmpEngine(), user, target, ContextData(), oid))
 
     if errorIndication:
-        print(f"SNMP v3 测试失败：{errorIndication}")
+        print(f"{ip} SNMP v3 测试失败：- {errorIndication}")
     elif errorStatus:
-        print(f"SNMP v3 测试失败：{errorStatus.prettyPrint()} at {errorIndex}")
+        print(f"{ip} SNMP v3 测试失败：- {errorStatus.prettyPrint()} at {errorIndex}")
     else:
-        print(f"SNMP v3 测试成功：{varBinds[0][1].prettyPrint()}")
-        # 获取设备名称
-        get_device_name(ip, username, auth_password, priv_password, auth_protocol, priv_protocol)
-
+        print(f"{ip} SNMP v3 测试成功：")
+        # print(f"SNMP v3 测试成功：{ip} - {varBinds[0][1].prettyPrint()}")
 
 # 获取设备名称
-def get_device_name(ip, username, auth_password, priv_password, auth_protocol, priv_protocol):
-    auth_protocol_map = {
-        'md5': usmHMACMD5AuthProtocol,
-        'sha': usmHMACSHAAuthProtocol
-    }
-    priv_protocol_map = {
-        'des': usmDESPrivProtocol,
-        'aes128': usmAesCfb128Protocol,
-        'aes192': usmAesCfb192Protocol,
-        'aes256': usmAesCfb256Protocol
-    }
-
-    auth_protocol = auth_protocol_map.get(auth_protocol, usmHMACSHAAuthProtocol)
-    priv_protocol = priv_protocol_map.get(priv_protocol, usmAesCfb256Protocol)
-
+def get_device_name(ip, username, auth_protocol, auth_password, priv_protocol, priv_password):
     target = UdpTransportTarget((ip, 161))
-    auth_data = UsmUserData(
-        userName=username,
-        authKey=auth_password,
-        privKey=priv_password,
-        authProtocol=auth_protocol,
-        privProtocol=priv_protocol
-    )
+    user = UsmUserData(userName=username,
+                       authProtocol=auth_protocol,
+                       authKey=auth_password,
+                       privProtocol=priv_protocol,
+                       privKey=priv_password)
+    oid = ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysName', 0))
 
-    errorIndication, errorStatus, errorIndex, varBinds = next(
-        getCmd(SnmpEngine(),
-               auth_data,
-               target,
-               ContextData(),
-               ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysName', 0)))
-    )
+    errorIndication, errorStatus, errorIndex, varBinds = next(getCmd(SnmpEngine(), user, target, ContextData(), oid))
 
     if errorIndication:
-        print(f"获取设备名称失败：{errorIndication}")
+        print(f"获取设备名称失败： {errorIndication}")
     elif errorStatus:
-        print(f"获取设备名称失败：{errorStatus.prettyPrint()} at {errorIndex}")
+        print(f"获取设备名称失败： {errorStatus.prettyPrint()} at {errorIndex}")
     else:
-        print(f"设备名称：{varBinds[0][1].prettyPrint()}")
+        print(f"设备名称： {varBinds[0][1].prettyPrint()}")
 
+# 读取IP地址列表文件
+ip_list = read_ip_list('2.ip_list.txt')
 
-# 测试SNMP v3
-ip = '192.168.56.10'
+# SNMP v3 配置信息
 username = 'snmp_immd'
-auth_password = 'P@ssw0rd'
-priv_password = 'P@ssw0rd'
-auth_protocol = 'sha'  # 认证协议，可选值为 'md5' 或 'sha'
-priv_protocol = 'aes256'  # 加密协议，可选值为 'des', 'aes128', 'aes192' 或 'aes256'
-test_snmp_v3(ip, username, auth_password, priv_password, auth_protocol, priv_protocol)
+auth_password = 'Password'
+priv_password = 'Password'
+auth_protocol = usmHMACSHAAuthProtocol
+priv_protocol = usmDESPrivProtocol
+# auth_protocol = usmHMACSHA2-256AuthProtocol
+# auth_protocol = usmHMACSHA2-512AuthProtocol
+# priv_protocol = usmAesCfb256Protocol
+
+# 使用多线程处理每个IP地址
+with ThreadPoolExecutor(max_workers=5) as executor:
+    for ip in ip_list:
+        executor.submit(test_snmp_v3, ip, username, auth_protocol, auth_password, priv_protocol, priv_password)
+        executor.submit(get_device_name, ip, username, auth_protocol, auth_password, priv_protocol, priv_password)
+
+
+
+#
+# S5700交换机配置
+# snmp-agent
+# snmp-agent trap enable
+# snmp-agent sys-info version v3
+# snmp-agent group v3 snmp_immd authentication
+# snmp-agent group v3 snmp_immd privacy  read-view iso write-view iso notify-view iso
+# snmp-agent mib-view included iso iso
+# snmp-agent usm-user v3 snmp_immd snmp_immd cipher authentication-mode sha Password privacy-mode des56 Password
+##
+
+#
+# CE12800交换机配置
+# snmp-agent
+# snmp-agent trap enable
+# snmp-agent sys-info version v3
+# snmp-agent group v3 snmp_immd authentication
+# snmp-agent mib-view included iso iso
+# snmp-agent usm-user v3 snmp_immd
+# snmp-agent usm-user v3 snmp_immd group snmp_immd
+# snmp-agent usm-user v3 snmp_immd authentication-mode sha
+# Password
+# Password
+# snmp-agent usm-user v3 snmp_immd privacy-mode des56
+# Password
+# Password
+# #

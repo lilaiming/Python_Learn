92badf3a3f4c81a19f46321e9cc611017ea548b7
lilaiming <441314423@qq.com>
2024-06-07 13:32:41 +0800

    list2

--- evpn_vxlan_no..py
+++ evpn_vxlan_no..py
@@ -7,31 +7,33 @@ import os
 template_text = """
 
 bridge-domain xxx
-vxlan vni 110xxx split-horizon-mode
-evpn binding vpn-instance AN_DEV bd-tag xxx
+vxlan vni 140xxx split-horizon-mode
+evpn binding vpn-instance MCN_DEV bd-tag xxx
 #
-interface Eth-Trunk 200.xxx mode l2
+interface Eth-Trunk 28.xxx mode l2
 encapsulation dot1q vid xxx
 rewrite pop single
 bridge-domain xxx
 #
 interface Nve1
-vni 110xxx head-end peer-list protocol bgp
+vni 140xxx head-end peer-list protocol bgp
 """
-
+# modify vni 110，AN_DEV|MCN_DEV，Eth-Trunk 200.
+# modify list vlan no.
+list= [
+483
+]
 
 list2 =[]
-list=[141,142]
 
 for i in list:
     str(i)
-    list2.append( str(i))
+    list2.append(str(i))
 print(list2)
-
 
 output_file = "C:/Users/lilm6/Desktop/output_folder/evpn_vxlan_no.txt"
 
-with open(output_file, 'w') as file:
+with open(output_file, 'a') as file:
     for number in list2:
         replaced_text = template_text.replace('xxx', number)
         file.write(replaced_text)
